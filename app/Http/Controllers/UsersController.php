774bf9ae848b7e03834dde6da3fb5f931ad09876
Bo <super90319@hotmail.com>
2018-09-05 00:59:44 +0000

    Basic user profile editing

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -3,10 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+Use App\Http\Requests\UserRequest;
 
 class UsersController extends Controller
 {
-    public funtion show(User $user){
-        return view('user.show', compact('user'));
+    public function show(User $user){
+        return view('users.show', compact('user'));
     }//
+
+    public function edit(User $user){
+        return view('users.edit', compact('user'));
+    }
+
+    public function update(UserRequest $request, User $user)
+    {
+        $user->update($request->all());
+        return redirect()->route('users.show', $user->id)->with('success', '个人资料更新成功！');
+    }
 }
